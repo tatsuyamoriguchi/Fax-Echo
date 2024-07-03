67c7719aef8b1757736bd3a2201063ab42a4d337
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2024-07-03 11:06:55 -0700

    Debugged 'Dashboard' initializer is inaccessible due to 'private' protection level -> removed private from var dateTimeFormatter = DateTimeFormatter()

---
+++ Fax Echo/View/Dashboard3.swift
@@ -0,0 +1,18 @@
+//
+//  Dashboard3.swift
+//  Fax Echo
+//
+//  Created by Tatsuya Moriguchi on 7/3/24.
+//
+
+import SwiftUI
+
+struct Dashboard3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Dashboard3()
+}
